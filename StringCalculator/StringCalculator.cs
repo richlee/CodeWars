9195ddff34d1f2f3dbd40a0c778f468baa9ace05
Rich <richalee@pm.me>
2022-05-22 13:36:46 +0100

    Add link to Roy's kata description

--- StringCalculator/StringCalculator.cs
+++ StringCalculator/StringCalculator.cs
@@ -1,5 +1,8 @@
 ﻿namespace StringCalculator;
 
+/// <summary>
+/// https://static1.squarespace.com/static/5c741968bfba3e13975e33a6/t/5ca6614d971a1877cadc4f8a/1554407757512/String+Calculator+Kata+v1.pdf
+/// </summary>
 public class StringCalculator
 {
     public event Action<string, int> AddOccurred = delegate { };
